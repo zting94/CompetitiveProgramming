6dc0510588a26596ac1721776b5dd9aa3ba7ff16
zting94 <kyk0128tt@gmail.com>
2021-05-16 00:01:54 +0900

    round_653 A B C D

---
+++ Cpp/Codeforce/round_653/C.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+}
+
+int main(int argc, char* argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int testCase = 1;
+    cin >> testCase;
+    for (int t = 0; t < testCase; ++t) {
+        solve();
+    }
+
+    return 0;
+}
