4dd9497a99791aed6aa642addad274e9c8a4eb51
zting94 <51961520+zting94@users.noreply.github.com>
2021-06-07 21:25:48 +0900

    Binary search (#54)
    * topological sort(graph)
    
    * N log N
    
    * N^2 log N

---
+++ Cpp/BaekJoon/2467.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+
+void solve() {
+    int N;
+    cin >> N;
+    vector<ll> arr(N);
+    for (auto& i : arr) {
+        cin >> i;
+    }
+    sort(arr.begin(), arr.end());
+    int x, y;
+    ll diff = LONG_MAX;
+    for (int i = 0; i < N; ++i) {
+        int idx = lower_bound(arr.begin(), arr.end(), -arr[i]) - arr.begin();
+        if (i != idx && idx < N) {
+            ll curDiff = abs(arr[i] + arr[idx]);
+            if (curDiff < diff) {
+                x = arr[i];
+                y = arr[idx];
+                diff = curDiff;
+            }
+        }
+        if (idx > 0 && idx - 1 != i) {
+            ll curDiff = abs(arr[i] + arr[idx - 1]);
+            if (curDiff < diff) {
+                x = arr[i];
+                y = arr[idx - 1];
+                diff = curDiff;
+            }
+        }
+        if (idx + 1 < N && idx + 1 != i) {
+            ll curDiff = abs(arr[i] + arr[idx + 1]);
+            if (curDiff < diff) {
+                x = arr[i];
+                y = arr[idx + 1];
+                diff = curDiff;
+            }
+        }
+    }
+    if (x > y) {
+        swap(x, y);
+    }
+    cout << x << ' ' << y << '\n';
+}
+
+int main(int argc, char* argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
