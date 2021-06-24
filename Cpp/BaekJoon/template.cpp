72f26c6024f0c8a6a00a04a7aada7bf8ba3d1df7
zting94 <51961520+zting94@users.noreply.github.com>
2021-06-24 09:12:36 +0900

    modify template.cpp(add template function to print, read) (#99)

--- Cpp/BaekJoon/template.cpp
+++ Cpp/BaekJoon/template.cpp
@@ -5,6 +5,26 @@ using namespace std;
 using ll = long long;
 using ull = unsigned long long;
 
+template<typename T>
+void print(const vector<T>& v, char sp=' ') {
+	for(int i=0; i<v.size(); ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+void print(int v) {
+	cout << v << '\n';
+}
+
+template<typename T>
+void read(vector<T>& v) {
+	for(auto& x : v)
+		cin >> x;
+}
+
 void solve() {
 }
 
