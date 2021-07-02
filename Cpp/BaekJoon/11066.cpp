ee2c4354e428172f9e26918987d684b89db787db
zting94 <kyk0128tt@gmail.com>
2021-07-03 00:27:35 +0900

    File Merge DP O(N^3) -> Can Optimize To O(N^2)

--- Cpp/BaekJoon/11066.cpp
+++ Cpp/BaekJoon/11066.cpp
@@ -2,37 +2,107 @@
 
 using namespace std;
 
-int cache[501][501];
-int arr[501];
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vii = vector<pii>;
+using vll = vector<pll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
 
-int dp(int left, int right) {
-    if (left > right) {
-        return 0;
-    }
-    if (left == right) {
-        return arr[left];
-    }
-    int& ret = cache[left][right];
-    if (ret != -1) {
-        return ret;
-    }
+template<typename T>
+void print(const vector<T>& v, char sp=' ') {
+	for(int i=0; i<v.size(); ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void print(const vector<vector<T>>& v, char sp=' ') {
+	for(auto& vv : v)
+		print(vv, sp);
+}
+
+template<typename T>
+void print(T v) {
+	cout << v << '\n';
+}
+
+template<typename T>
+void print(T v[], int sz, char sp=' ') {
+	for(int i=0; i<sz; ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void read(vector<T>& v) {
+	for(auto& x : v)
+		cin >> x;
+}
+
+template<typename T>
+void read(vector<vector<T>>& v) {
+	for(auto& a : v)
+		read(a);
+}
+
+template<typename T>
+void read(T a[], int sz) {
+	for(int i=0; i<sz; ++i)
+		cin >> a[i];
+}
+
+const int mxN=501;
+const int mx=1e8;
+int a[mxN];
+int sum[mxN];
+int cache[mxN][mxN];
+int n;
+
+int dp(int l, int r) {
+	if(l+1==r)
+		return a[l]+a[r];
+	if(l>=r)
+		return 0;
+	int& ret=cache[l][r];
+	if(ret!=-1)
+		return ret;
+	ret=mx;
+	for(int i=l; i<=r; ++i) {
+		ret=min(ret, dp(l, i)+dp(i+1, r));
+	}
+	return ret+=sum[r+1]-sum[max(l, 0)];
 }
 
 void solve() {
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
-    }
-    memset(cache, -1, sizeof(cache));
-    cout << dp(0, n - 1) << '\n';
+	cin >> n;
+	memset(cache, -1, sizeof(cache));
+	sum[0]=0;
+	for(int i=0; i<n; ++i) {
+		cin >> a[i];
+		sum[i+1]=sum[i]+a[i];
+	}
+	print(dp(0, n-1));
 }
 
 int main(int argc, char* argv[]) {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    solve();
+	int tt;
+	cin >> tt;
+	while(tt--)
+    	solve();
 
     return 0;
 }
