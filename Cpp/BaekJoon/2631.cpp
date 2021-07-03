1cf0626f58dccda4db66d680c649ebdc2022235b
zting94 <kyk0128tt@gmail.com>
2021-07-04 00:26:45 +0900

    줄세우기 문제(LIS 를 전체 개수에서 빼주면 됨)

---
+++ Cpp/BaekJoon/2631.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vii = vector<pii>;
+using vll = vector<pll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+
+template<typename T>
+void print(const vector<T>& v, char sp=' ') {
+	for(int i=0; i<v.size(); ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void print(const vector<vector<T>>& v, char sp=' ') {
+	for(auto& vv : v)
+		print(vv, sp);
+}
+
+template<typename T>
+void print(T v) {
+	cout << v << '\n';
+}
+
+template<typename T>
+void print(T v[], int sz, char sp=' ') {
+	for(int i=0; i<sz; ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void read(vector<T>& v) {
+	for(auto& x : v)
+		cin >> x;
+}
+
+template<typename T>
+void read(vector<vector<T>>& v) {
+	for(auto& a : v)
+		read(a);
+}
+
+template<typename T>
+void read(T a[], int sz) {
+	for(int i=0; i<sz; ++i)
+		cin >> a[i];
+}
+
+void solve() {
+	int n;
+	cin >> n;
+	vi a(n), dp(n, 1e9), l{-1};
+	read(a);
+	for(int i=0; i<n; ++i) {
+		if(a[i]>l.back()) 
+			l.push_back(a[i]);
+		else {
+			int j=lower_bound(l.begin(), l.end(), a[i])-l.begin();
+			l[j]=a[i];
+		}
+	}
+	print(n-l.size()+1);
+}
+
+int main(int argc, char* argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
