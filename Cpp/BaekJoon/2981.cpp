d9947fe7a6a185f8c44d54a53af659869d4d7edd
zting94 <51961520+zting94@users.noreply.github.com>
2021-07-06 09:41:12 +0900

    GCD & Prime (#146)
    Co-authored-by: geunyoung <young@geunyoungui-MacBookAir.local>

---
+++ Cpp/BaekJoon/2981.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vii = vector<pii>;
+using vll = vector<pll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+template<typename T>
+void print(const vector<T>& v, char sp=' ') {
+	for(int i=0; i<v.size(); ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void print(const vector<vector<T>>& v, char sp=' ') {
+	for(auto& vv : v)
+		print(vv, sp);
+}
+
+template<typename T>
+void print(T v) {
+	cout << v << '\n';
+}
+
+template<typename T>
+void print(T v[], int sz, char sp=' ') {
+	for(int i=0; i<sz; ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void read(vector<T>& v) {
+	for(auto& x : v)
+		cin >> x;
+}
+
+template<typename T>
+void read(vector<vector<T>>& v) {
+	for(auto& a : v)
+		read(a);
+}
+
+template<typename T>
+void read(T a[], int sz) {
+	for(int i=0; i<sz; ++i)
+		cin >> a[i];
+}
+
+void solve() {
+	int n;
+	cin >> n;
+	vector<ull> a(n);
+	read(a);
+	sort(all(a));
+	ull g=a[1]-a[0];
+	for(int i=2; i<n;++i) 
+		g=__gcd(g, a[i]-a[i-1]);
+	vector<int> p;
+	for(int i=2; i*i<=g; ++i) {
+		if(g%i==0) {
+			p.push_back(i);
+			p.push_back(g/i);
+		}
+	}
+	p.push_back(g);
+	sort(all(p));
+	p.erase(unique(all(p)), p.end());
+	print(p);
+}
+
+int main(int argc, char* argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
