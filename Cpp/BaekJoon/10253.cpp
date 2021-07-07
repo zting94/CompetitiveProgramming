d44365db56d94e412c1dcb254ff3b48b7ebd3f1a
zting94 <kyk0128tt@gmail.com>
2021-07-07 21:47:03 +0900

    Math Problems

---
+++ Cpp/BaekJoon/10253.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vii = vector<pii>;
+using vll = vector<pll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+template<typename T>
+void print(const vector<T>& v, char sp=' ') {
+	for(int i=0; i<v.size(); ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void print(const vector<vector<T>>& v, char sp=' ') {
+	for(auto& vv : v)
+		print(vv, sp);
+}
+
+template<typename T>
+void print(T v) {
+	cout << v << '\n';
+}
+
+template<typename T>
+void print(T v[], int sz, char sp=' ') {
+	for(int i=0; i<sz; ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void read(vector<T>& v) {
+	for(auto& x : v)
+		cin >> x;
+}
+
+template<typename T>
+void read(vector<vector<T>>& v) {
+	for(auto& a : v)
+		read(a);
+}
+
+template<typename T>
+void read(T a[], int sz) {
+	for(int i=0; i<sz; ++i)
+		cin >> a[i];
+}
+
+void solve() {
+	ll a, b, c=1;
+	cin >> a >> b;
+	while(a) {
+		if(a==1) {
+			c=b;
+			break;
+		}
+		for(ll i=c+1;;++i) {
+			ll g=__gcd(b, i);
+			ll t1=a*i/g;
+			ll t2=b/g;
+			if(t1>=t2) {
+				b*=i/g;
+				a=t1-t2;
+				c=i;
+				ll gg=__gcd(b, a);
+				a/=gg;
+				b/=gg;
+				break;
+			}
+		}
+	}
+	assert(c!=1);
+	print(c);
+}
+
+int main(int argc, char* argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+	int t;
+	cin >> t;
+	while(t--)
+    	solve();
+
+    return 0;
+}
