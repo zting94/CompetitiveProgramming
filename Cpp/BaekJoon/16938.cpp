a4ce5ec9093d6ef2136a28b58ab4f823b0430b15
zting94 <51961520+zting94@users.noreply.github.com>
2021-07-09 16:32:05 +0900

    Brute Force with __builtin_popcount (#160)

---
+++ Cpp/BaekJoon/16938.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vii = vector<pii>;
+using vll = vector<pll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+template<typename T>
+void print(const vector<T>& v, char sp=' ') {
+	for(int i=0; i<v.size(); ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void print(const vector<vector<T>>& v, char sp=' ') {
+	for(auto& vv : v)
+		print(vv, sp);
+}
+
+template<typename T>
+void print(T v) {
+	cout << v << '\n';
+}
+
+template<typename T>
+void print(T v[], int sz, char sp=' ') {
+	for(int i=0; i<sz; ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void read(vector<T>& v) {
+	for(auto& x : v)
+		cin >> x;
+}
+
+template<typename T>
+void read(vector<vector<T>>& v) {
+	for(auto& a : v)
+		read(a);
+}
+
+template<typename T>
+void read(T a[], int sz) {
+	for(int i=0; i<sz; ++i)
+		cin >> a[i];
+}
+
+void solve() {
+	int N, L, R, X;
+	cin >> N >> L >> R >> X;
+	vi A(N);
+	read(A);
+	sort(all(A));
+	int ans=0;
+	for(int i=1; i<(1<<N); ++i) {
+		int cnt=__builtin_popcount(i);
+		if(cnt<2)
+			continue;
+		int mn=INT_MAX, mx=INT_MIN;
+		int s=0;
+		for(int j=0; j<N; ++j) {
+			if(i&(1<<j)) {
+				s+=A[j];
+				mn=min(mn, A[j]);
+				mx=max(mx, A[j]);
+			}
+		}
+		if(s>=L&&R>=s&&(mx-mn)>=X)
+			++ans;
+	}
+	print(ans);
+}
+
+int main(int argc, char* argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
