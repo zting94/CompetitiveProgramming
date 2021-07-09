cea9ec25a2dd025d3a98c7250b6e4a6440009d3a
zting94 <51961520+zting94@users.noreply.github.com>
2021-07-10 00:17:40 +0900

    Priority Queue && Prime Multiplication (#163)
    Co-authored-by: geunyoung <young@geunyoungui-MacBookAir.local>

---
+++ Cpp/BaekJoon/2014.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vii = vector<pii>;
+using vll = vector<pll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+template<typename T>
+void print(const vector<T>& v, char sp=' ') {
+	for(int i=0; i<v.size(); ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void print(const vector<vector<T>>& v, char sp=' ') {
+	for(auto& vv : v)
+		print(vv, sp);
+}
+
+template<typename T>
+void print(T v) {
+	cout << v << '\n';
+}
+
+template<typename T>
+void print(T v[], int sz, char sp=' ') {
+	for(int i=0; i<sz; ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void read(vector<T>& v) {
+	for(auto& x : v)
+		cin >> x;
+}
+
+template<typename T>
+void read(vector<vector<T>>& v) {
+	for(auto& a : v)
+		read(a);
+}
+
+template<typename T>
+void read(T a[], int sz) {
+	for(int i=0; i<sz; ++i)
+		cin >> a[i];
+}
+
+void solve() {
+	int K, N;
+	cin >> K >> N;
+	vi a(K);
+	read(a);
+	priority_queue<ll, vl, greater<ll>> pq(all(a));
+	int ans=-1;
+	for(int i=0; i<N; ++i) {
+		ans=pq.top();
+		pq.pop();
+		for(int p : a) {
+			pq.push(p*1LL*ans);
+			if(ans%p==0)
+				break;
+		}
+	}
+	assert(ans!=-1);
+	print(ans);
+}
+
+int main(int argc, char* argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
