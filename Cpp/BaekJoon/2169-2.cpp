c15e692ac2306dd7043e3fdad081a640b49f28ec
zting94 <51961520+zting94@users.noreply.github.com>
2021-07-15 16:20:56 +0900

    2D + direction DP (#182)

---
+++ Cpp/BaekJoon/2169-2.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vii = vector<pii>;
+using vll = vector<pll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+template<typename T>
+void print(const vector<T>& v, char sp=' ') {
+	for(int i=0; i<v.size(); ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void print(const vector<vector<T>>& v, char sp=' ') {
+	for(auto& vv : v)
+		print(vv, sp);
+}
+
+template<typename T>
+void print(T v) {
+	cout << v << '\n';
+}
+
+template<typename T>
+void print(T v[], int sz, char sp=' ') {
+	for(int i=0; i<sz; ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void read(vector<T>& v) {
+	for(auto& x : v)
+		cin >> x;
+}
+
+template<typename T>
+void read(vector<vector<T>>& v) {
+	for(auto& a : v)
+		read(a);
+}
+
+template<typename T>
+void read(T a[], int sz) {
+	for(int i=0; i<sz; ++i)
+		cin >> a[i];
+}
+
+const int mxN=1e3;
+int dp[mxN][mxN], ldp[mxN], rdp[mxN], a[mxN][mxN], N, M;
+
+void solve() {
+	cin >> N >> M;
+	for(int i=0; i<N; ++i)
+		for(int j=0; j<M; ++j)
+			cin >> a[i][j];
+	dp[0][0]=a[0][0];
+	for(int j=1; j<M; ++j)
+		dp[0][j]=dp[0][j-1]+a[0][j];
+	for(int i=1; i<N; ++i) {
+		ldp[0]=dp[i-1][0]+a[i][0];
+		rdp[M-1]=dp[i-1][M-1]+a[i][M-1];
+		for(int j=1; j<M; ++j)
+			ldp[j]=max(dp[i-1][j], ldp[j-1])+a[i][j];
+		for(int j=M-2; j>=0; --j)
+			rdp[j]=max(dp[i-1][j], rdp[j+1])+a[i][j];
+		for(int j=0; j<M; ++j)
+			dp[i][j]=max(rdp[j], ldp[j]);
+	}
+	print(dp[N-1][M-1]);
+}
+
+int main(int argc, char* argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
