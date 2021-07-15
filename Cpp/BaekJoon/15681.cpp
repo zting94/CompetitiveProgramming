110e088cbaa940a698656288e8e2cc5f154a2a74
zting94 <kyk0128tt@gmail.com>
2021-07-15 16:41:07 +0900

    Basic Tree DFS

---
+++ Cpp/BaekJoon/15681.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vii = vector<pii>;
+using vll = vector<pll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+template<typename T>
+void print(const vector<T>& v, char sp=' ') {
+	for(int i=0; i<v.size(); ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void print(const vector<vector<T>>& v, char sp=' ') {
+	for(auto& vv : v)
+		print(vv, sp);
+}
+
+template<typename T>
+void print(T v) {
+	cout << v << '\n';
+}
+
+template<typename T>
+void print(T v[], int sz, char sp=' ') {
+	for(int i=0; i<sz; ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void read(vector<T>& v) {
+	for(auto& x : v)
+		cin >> x;
+}
+
+template<typename T>
+void read(vector<vector<T>>& v) {
+	for(auto& a : v)
+		read(a);
+}
+
+template<typename T>
+void read(T a[], int sz) {
+	for(int i=0; i<sz; ++i)
+		cin >> a[i];
+}
+
+const int mxN=1e5;
+int N, R, Q, cnt[mxN];
+vi adj[mxN];
+
+int dfs(int u, int p) {
+	int ret=1;
+	for(int v : adj[u]) 
+		if(v!=p)
+			ret+=dfs(v, u);
+	cnt[u]=ret;
+	return ret;
+}
+
+void solve() {
+	cin >> N >> R >> Q, --R;
+	for(int i=0; i<N-1; ++i) {
+		int u, v;
+		cin >> u >> v, --u, --v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	cnt[R]=dfs(R, R);
+	for(int i=0, v; i<Q; ++i) {
+		cin >> v;
+		print(cnt[v-1]);
+	}
+}
+
+int main(int argc, char* argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
