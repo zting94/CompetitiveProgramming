6e780718b47a5342c87171671d6e37ac6dcd7a7b
zting94 <kyk0128tt@gmail.com>
2021-08-10 00:10:17 +0900

    Linear DP

---
+++ Cpp/BaekJoon/2491.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vii = vector<pii>;
+using vll = vector<pll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+template<typename T>
+void print(const vector<T>& v, char sp=' ') {
+	for(int i=0; i<v.size(); ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void print(const vector<vector<T>>& v, char sp=' ') {
+	for(auto& vv : v)
+		print(vv, sp);
+}
+
+template<typename T>
+void print(T v) {
+	cout << v << '\n';
+}
+
+template<typename T>
+void print(T v[], int sz, char sp=' ') {
+	for(int i=0; i<sz; ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void read(vector<T>& v) {
+	for(auto& x : v)
+		cin >> x;
+}
+
+template<typename T>
+void read(vector<vector<T>>& v) {
+	for(auto& a : v)
+		read(a);
+}
+
+template<typename T>
+void read(T a[], int sz) {
+	for(int i=0; i<sz; ++i)
+		cin >> a[i];
+}
+
+int DP(vi a) {
+	int n=a.size(), ret=0;
+	a.insert(a.begin(), 10);
+	vi dp(n+1);
+	for(int i=1; i<=n; ++i) {
+		if(a[i]<a[i-1])
+			dp[i]=1;
+		else
+			dp[i]=dp[i-1]+1;
+		ret=max(ret, dp[i]);
+	}
+	return ret;
+}
+
+int DP2(vi a) {
+	int n=a.size(), ret=0;
+	a.insert(a.begin(), 0);
+	vi dp(n+1);
+	for(int i=1; i<=n; ++i) {
+		if(a[i]>a[i-1])
+			dp[i]=1;
+		else
+			dp[i]=dp[i-1]+1;
+		ret=max(ret, dp[i]);
+	}
+	return ret;
+}
+
+void solve() {
+	int n;
+	cin >> n;
+	vi a(n);
+	read(a);
+	print(max(DP(a), DP2(a)));
+}
+
+int main(int argc, char* argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
