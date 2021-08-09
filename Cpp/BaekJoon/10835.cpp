a3b2940522ae340fdc97c8c874ec52fe7bb371a2
zting94 <51961520+zting94@users.noreply.github.com>
2021-08-10 00:07:30 +0900

    Recursive 2D DP (#212)

---
+++ Cpp/BaekJoon/10835.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vii = vector<pii>;
+using vll = vector<pll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+template<typename T>
+void print(const vector<T>& v, char sp=' ') {
+	for(int i=0; i<v.size(); ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void print(const vector<vector<T>>& v, char sp=' ') {
+	for(auto& vv : v)
+		print(vv, sp);
+}
+
+template<typename T>
+void print(T v) {
+	cout << v << '\n';
+}
+
+template<typename T>
+void print(T v[], int sz, char sp=' ') {
+	for(int i=0; i<sz; ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void read(vector<T>& v) {
+	for(auto& x : v)
+		cin >> x;
+}
+
+template<typename T>
+void read(vector<vector<T>>& v) {
+	for(auto& a : v)
+		read(a);
+}
+
+template<typename T>
+void read(T a[], int sz) {
+	for(int i=0; i<sz; ++i)
+		cin >> a[i];
+}
+
+const int mxN=2e3+1;
+int n, a[mxN], b[mxN], dp[mxN][mxN];
+
+int dfs(int i, int j) {
+	if(i>=n||j>=n)
+		return 0;
+	int& ret=dp[i][j];
+	if(ret!=-1)
+		return ret;
+	ret=dfs(i+1, j);
+	ret=max(ret, dfs(i+1, j+1));
+	if(a[i]>b[j])
+		ret=max(ret, dfs(i, j+1)+b[j]);
+	return ret;
+}
+
+void solve() {
+	memset(dp, -1, sizeof(dp));
+	cin >> n;
+	read(a, n);
+	read(b, n);
+	print(dfs(0, 0));
+}
+
+int main(int argc, char* argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
