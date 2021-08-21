a5eaf05f5257bc2aef62cc207e2b6012704f1dca
zting94 <kyk0128tt@gmail.com>
2021-08-22 00:09:16 +0900

    Dijkstra + Custom

---
+++ Cpp/BaekJoon/13308.cpp
@@ -0,0 +1,159 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vii = vector<pii>;
+using vll = vector<pll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+
+void __zbg(int v) { cerr << v; }
+void __zbg(long v) { cerr << v; }
+void __zbg(ll v) { cerr << v; }
+void __zbg(unsigned v) { cerr << v; }
+void __zbg(unsigned long v) { cerr << v; }
+void __zbg(ull v) { cerr << v; }
+void __zbg(float v) { cerr << v; }
+void __zbg(double v) { cerr << v; }
+void __zbg(long double v) { cerr << v; }
+void __zbg(char v) { cerr << '\'' << v << '\''; }
+void __zbg(const char* v) { cerr << '\'' << v << '\''; }
+void __zbg(const string& v) { cerr << '\'' << v << '\''; }
+void __zbg(bool v) { cerr << v?"true":"false"; }
+
+template<typename T, typename V>
+void __zbg(const pair<T, V>& v) {
+	cerr << '{';
+	__zbg(v.first);
+	cerr << ", ";
+	__zbg(v.second);
+	cerr << '}';
+}
+
+template<typename T>
+void __zbg(const T& x) {
+	int f=0;
+	cerr << '{';
+	for (auto &i : x) {
+		cerr << (f++?", ":"");
+		__zbg(i);
+	}
+	cerr << '}';
+}
+
+void _zbg() { cerr << "]\n"; }
+
+template<typename T, typename... V>
+void _zbg(T t, V... v) {
+	__zbg(t);
+	if(sizeof...(v))
+		cerr << ", ";
+	_zbg(v...);
+}
+//#define dbg(x...) cerr << "\033[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _zbg(x); cerr << "\033[39m" << endl;
+#define dbg(x...) cerr << __func__<<":"<<__LINE__<<" [" << #x << "] = ["; _zbg(x); cerr << endl;
+
+template<typename T>
+void print(const vector<T>& v, char sp=' ') {
+	for(int i=0; i<v.size(); ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void print(const vector<vector<T>>& v, char sp=' ') {
+	for(auto& vv : v)
+		print(vv, sp);
+}
+
+template<typename T>
+void print(T v) {
+	cout << v << '\n';
+}
+
+template<typename T>
+void print(T v[], int sz, char sp=' ') {
+	for(int i=0; i<sz; ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void read(vector<T>& v) {
+	for(auto& x : v)
+		cin >> x;
+}
+
+template<typename T>
+void read(vector<vector<T>>& v) {
+	for(auto& a : v)
+		read(a);
+}
+
+template<typename T>
+void read(T a[], int sz) {
+	for(int i=0; i<sz; ++i)
+		cin >> a[i];
+}
+
+class Edge {
+public :
+	int u, c;
+	ll d;
+	bool operator<(const Edge& e) const {
+		return d>e.d;
+	}
+};
+
+void solve() {
+	int n, m;
+	cin >> n >> m;
+	vi cost(n);
+	vector<vii> adj(n);
+	vector<vector<bool>> vis(3001, vector<bool>(3001, false));
+	read(cost);
+	for(int i=0; i<m; ++i) {
+		int u, v, c;
+		cin >> u >> v >> c, --u, --v;
+		adj[u].push_back({v, c});
+		adj[v].push_back({u, c});
+	}
+	priority_queue<Edge> pq;
+	pq.push({0, cost[0], 0});
+	// 언제나 지금까지 지나오면서 가장 값 싼 도시에서
+	// 기름을 많이 넣는 것이 유리함
+	while(!pq.empty()) {
+		auto [u, c, d]=pq.top();
+		pq.pop();
+		if(vis[u][c]) 
+			continue;
+		if(u==n-1) {
+			print(d);
+			return;
+		}
+		vis[u][c]=1;
+		for(auto [v, nd] : adj[u]) 
+			pq.push({v, min(c, cost[v]), c*nd+d});
+	}
+}
+
+int main(int argc, char* argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
