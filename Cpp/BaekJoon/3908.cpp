deb7e59da3adbb71f3fcbf1dbb68b6a102a4e41c
zting94 <kyk0128tt@gmail.com>
2021-09-18 22:58:35 +0900

    DP + Prime Check

---
+++ Cpp/BaekJoon/3908.cpp
@@ -0,0 +1,158 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vii = vector<pii>;
+using vll = vector<pll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+
+void __zbg(int v) { cerr << v; }
+void __zbg(long v) { cerr << v; }
+void __zbg(ll v) { cerr << v; }
+void __zbg(unsigned v) { cerr << v; }
+void __zbg(unsigned long v) { cerr << v; }
+void __zbg(ull v) { cerr << v; }
+void __zbg(float v) { cerr << v; }
+void __zbg(double v) { cerr << v; }
+void __zbg(long double v) { cerr << v; }
+void __zbg(char v) { cerr << '\'' << v << '\''; }
+void __zbg(const char* v) { cerr << '\'' << v << '\''; }
+void __zbg(const string& v) { cerr << '\'' << v << '\''; }
+void __zbg(bool v) { cerr << (v?"true":"false"); }
+
+template<typename T, typename V>
+void __zbg(const pair<T, V>& v) {
+	cerr << '{';
+	__zbg(v.first);
+	cerr << ", ";
+	__zbg(v.second);
+	cerr << '}';
+}
+
+template<typename T>
+void __zbg(const T& x) {
+	int f=0;
+	cerr << '{';
+	for (auto &i : x) {
+		cerr << (f++?", ":"");
+		__zbg(i);
+	}
+	cerr << '}';
+}
+
+void _zbg() { cerr << "]\n"; }
+
+template<typename T, typename... V>
+void _zbg(T t, V... v) {
+	__zbg(t);
+	if(sizeof...(v))
+		cerr << ", ";
+	_zbg(v...);
+}
+//#define dbg(x...) cerr << "\033[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _zbg(x); cerr << "\033[39m" << endl;
+#define dbg(x...) cerr << __func__<<":"<<__LINE__<<" [" << #x << "] = ["; _zbg(x); cerr << endl;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).begin(), (x).end()
+#define nl '\n'
+
+template<typename T>
+void print(const vector<T>& v, char sp=' ') {
+	for(int i=0; i<v.size(); ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void print(const vector<vector<T>>& v, char sp=' ') {
+	for(auto& vv : v)
+		print(vv, sp);
+}
+
+template<typename T>
+void print(T v) {
+	cout << v << '\n';
+}
+
+template<typename T>
+void print(T v[], int sz, char sp=' ') {
+	for(int i=0; i<sz; ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void read(vector<T>& v) {
+	for(auto& x : v)
+		cin >> x;
+}
+
+template<typename T>
+void read(vector<vector<T>>& v) {
+	for(auto& a : v)
+		read(a);
+}
+
+template<typename T>
+void read(T a[], int sz) {
+	for(int i=0; i<sz; ++i)
+		cin >> a[i];
+}
+
+const int mxN=1121, mxK=15;
+int dp[mxN][mxN][mxK];
+bool v[mxN];
+vi p;
+
+int dfs(int i, int n, int k) {
+	if(k==0)
+		return n==0;
+	if(i==p.size()||n<=0)
+		return 0;
+	int& ret=dp[i][n][k];
+	if(ret!=-1)
+		return ret;
+	if(p[i]>n)
+		return ret=0;
+	ret=dfs(i+1, n, k);
+	return ret+=dfs(i+1, n-p[i], k-1);
+}
+
+void solve() {
+	memset(dp, -1, sizeof(dp));
+	p.push_back(2);
+	for(int i=3; i<mxN; i+=2) {
+		if(!v[i]) {
+			p.push_back(i);
+			for(int j=i; j<mxN; j+=i)
+				v[j]=1;
+		}
+	}
+	int tt, n, k;
+	cin >> tt; 
+	while(tt--) {
+		cin >> n >> k;
+		print(dfs(0, n, k));
+	}
+}
+
+int main(int argc, char* argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
