2a6120b309404c1313ab1afe8230e0c8d25ba996
zting94 <51961520+zting94@users.noreply.github.com>
2021-09-18 23:03:07 +0900

    DP Maximum Rectangle (#234)

---
+++ Cpp/BaekJoon/4095.cpp
@@ -0,0 +1,154 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vii = vector<pii>;
+using vll = vector<pll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+
+void __zbg(int v) { cerr << v; }
+void __zbg(long v) { cerr << v; }
+void __zbg(ll v) { cerr << v; }
+void __zbg(unsigned v) { cerr << v; }
+void __zbg(unsigned long v) { cerr << v; }
+void __zbg(ull v) { cerr << v; }
+void __zbg(float v) { cerr << v; }
+void __zbg(double v) { cerr << v; }
+void __zbg(long double v) { cerr << v; }
+void __zbg(char v) { cerr << '\'' << v << '\''; }
+void __zbg(const char* v) { cerr << '\'' << v << '\''; }
+void __zbg(const string& v) { cerr << '\'' << v << '\''; }
+void __zbg(bool v) { cerr << (v?"true":"false"); }
+
+template<typename T, typename V>
+void __zbg(const pair<T, V>& v) {
+	cerr << '{';
+	__zbg(v.first);
+	cerr << ", ";
+	__zbg(v.second);
+	cerr << '}';
+}
+
+template<typename T>
+void __zbg(const T& x) {
+	int f=0;
+	cerr << '{';
+	for (auto &i : x) {
+		cerr << (f++?", ":"");
+		__zbg(i);
+	}
+	cerr << '}';
+}
+
+void _zbg() { cerr << "]\n"; }
+
+template<typename T, typename... V>
+void _zbg(T t, V... v) {
+	__zbg(t);
+	if(sizeof...(v))
+		cerr << ", ";
+	_zbg(v...);
+}
+//#define dbg(x...) cerr << "\033[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _zbg(x); cerr << "\033[39m" << endl;
+#define dbg(x...) cerr << __func__<<":"<<__LINE__<<" [" << #x << "] = ["; _zbg(x); cerr << endl;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).begin(), (x).end()
+#define nl '\n'
+
+template<typename T>
+void print(const vector<T>& v, char sp=' ') {
+	for(int i=0; i<v.size(); ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void print(const vector<vector<T>>& v, char sp=' ') {
+	for(auto& vv : v)
+		print(vv, sp);
+}
+
+template<typename T>
+void print(T v) {
+	cout << v << '\n';
+}
+
+template<typename T>
+void print(T v[], int sz, char sp=' ') {
+	for(int i=0; i<sz; ++i) {
+		if(i>0)
+			cout << sp;
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void read(vector<T>& v) {
+	for(auto& x : v)
+		cin >> x;
+}
+
+template<typename T>
+void read(vector<vector<T>>& v) {
+	for(auto& a : v)
+		read(a);
+}
+
+template<typename T>
+void read(T a[], int sz) {
+	for(int i=0; i<sz; ++i)
+		cin >> a[i];
+}
+
+const int mxN=1e3+1;
+int N, M, dp[mxN][mxN], A[mxN][mxN];
+
+void solve() {
+	memset(dp, 0, sizeof(dp));
+	for(int i=0; i<N; ++i)
+		for(int j=0; j<M; ++j)
+			cin >> A[i][j];
+	int ans=0;
+	for(int i=0; i<M; ++i) {
+		dp[0][i]=A[0][i]==1;
+		ans=max(ans, dp[0][i]);
+	}
+	for(int i=0; i<N; ++i) {
+		dp[i][0]=A[i][0]==1;
+		ans=max(ans, dp[i][0]);
+	}
+	for(int i=1; i<N; ++i) {
+		for(int j=1; j<M; ++j) {
+			if(A[i][j]==0)
+				continue;
+			dp[i][j]=min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1])+1;
+			ans=max(ans, dp[i][j]);
+		}
+	}
+	print(ans);
+}
+
+int main(int argc, char* argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+	while(1) {
+		cin >> N >> M;
+		if(N==0&&M==0)
+			break;
+    	solve();
+	}
+
+    return 0;
+}
