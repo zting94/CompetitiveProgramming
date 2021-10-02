a4b6fa8688017bf230dc49fc6152fd19ee841d51
zting94 <51961520+zting94@users.noreply.github.com>
2021-10-02 16:35:36 +0900

    Simple Silver (#236)

--- Cpp/BaekJoon/_template.cpp
+++ Cpp/BaekJoon/_template.cpp
@@ -2,6 +2,24 @@
 
 using namespace std;
 
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vii = vector<pii>;
+using vll = vector<pll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).begin(), (x).end()
+#define nl '\n'
+
+const int dx[4]={-1, 0, 1, 0}, dy[4]={0, -1, 0, 1};
+
+
 void solve() {
 }
 
@@ -9,11 +27,7 @@ int main(int argc, char* argv[]) {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int testCase = 1;
-    cin >> testCase;
-    for(int t = 0; t < testCase; ++t)  {
-       solve();
-    }
+	solve();
 
     return 0;
 }
